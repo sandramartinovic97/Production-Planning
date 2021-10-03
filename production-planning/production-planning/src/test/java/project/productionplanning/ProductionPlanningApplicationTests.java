2570522a8104eb732dea70aab0a1f3f9302e054c
sandramartinovic97 <sandramartinovic.sm@gmail.com>
2021-10-03 17:09:19 +0200

    Initial commit

---
+++ production-planning/production-planning/src/test/java/project/productionplanning/ProductionPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package project.productionplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
