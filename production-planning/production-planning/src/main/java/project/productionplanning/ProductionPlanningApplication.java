50df7617b93ffb2430e8ee91ed8e1a04b0e53cc5
sandramartinovic97 <sandramartinovic.sm@gmail.com>
2021-10-09 00:46:17 +0200

    Updated class ProductionPlanningApplication.

--- production-planning/production-planning/src/main/java/project/productionplanning/ProductionPlanningApplication.java
+++ production-planning/production-planning/src/main/java/project/productionplanning/ProductionPlanningApplication.java
@@ -2,19 +2,12 @@ package project.productionplanning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 
-@SpringBootApplication //(exclude = SqlInitializationAutoConfiguration.class)
+@SpringBootApplication
 public class ProductionPlanningApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductionPlanningApplication.class, args);
 	}
 
-	/*@Bean
-	public ServletRegistrationBean h2servletRegistration() {
-		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-		registrationBean.addUrlMappings("/h2-console/*");
-		return registrationBean;
-	}*/
 }
