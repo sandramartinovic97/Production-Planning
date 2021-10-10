28450cdf55f3e9d7567035c64217bff0aa2b20f0
sandramartinovic97 <sandramartinovic.sm@gmail.com>
2021-10-10 15:31:36 +0200

    Implemented methods for adding, updating and deleting Supplier.

--- production-planning/production-planning/src/main/java/project/productionplanning/controller/SupplierController.java
+++ production-planning/production-planning/src/main/java/project/productionplanning/controller/SupplierController.java
@@ -1,10 +1,7 @@
 package project.productionplanning.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.productionplanning.dto.SupplierDto;
 import project.productionplanning.service.SupplierService;
 
@@ -25,5 +22,20 @@ public class SupplierController {
     @GetMapping("/{id}")
     public SupplierDto getSupplierById(@PathVariable("id") Integer id) {
         return supplierService.getSupplierById(id);
+    }
+
+    @PostMapping
+    public void addSupplier(@RequestBody SupplierDto supplierDto){
+        supplierService.addSupplier(supplierDto);
+    }
+
+    @PutMapping("/{id}")
+    public void updateSupplier(@RequestBody SupplierDto supplierDto, @PathVariable("id") Integer id){
+        supplierService.updateSupplier(supplierDto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteSupplier(@PathVariable("id") Integer id) {
+        supplierService.deleteSupplier(id);
     }
 }
--- production-planning/production-planning/src/main/java/project/productionplanning/service/SupplierServiceImpl.java
+++ production-planning/production-planning/src/main/java/project/productionplanning/service/SupplierServiceImpl.java
@@ -2,6 +2,7 @@ package project.productionplanning.service;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import project.productionplanning.dto.SupplierDto;
 import project.productionplanning.model.Supplier;
@@ -16,6 +17,8 @@ public class SupplierServiceImpl implements SupplierService {
     @Autowired
     private SupplierRepository supplierRepository;
     private ModelMapper modelMapper = new ModelMapper();
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Override
     public List<SupplierDto> getAllSuppliers() {
@@ -36,16 +39,27 @@ public class SupplierServiceImpl implements SupplierService {
 
     @Override
     public SupplierDto addSupplier(SupplierDto supplierDto) {
-        return null;
+        Supplier supplier = modelMapper.map(supplierDto, Supplier.class);
+        supplierRepository.save(supplier);
+        return modelMapper.map(supplier, SupplierDto.class);
     }
 
     @Override
     public SupplierDto updateSupplier(SupplierDto supplierDto, Integer id) {
-        return null;
+        Supplier updatedSupplier = modelMapper.map(supplierDto, Supplier.class);
+        Supplier supplierFromDB = supplierRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Could not find supplier with specified id = " + id));
+        supplierFromDB.setSupplierName(updatedSupplier.getSupplierName());
+        supplierFromDB.setSupplierEmail(updatedSupplier.getSupplierEmail());
+        supplierFromDB.setSupplierPhone(updatedSupplier.getSupplierPhone());
+        supplierFromDB.setSupplierCountry(updatedSupplier.getSupplierCountry());
+        supplierFromDB.setSupplierCity(updatedSupplier.getSupplierCity());
+        supplierFromDB.setSupplierAddress(updatedSupplier.getSupplierAddress());
+        return modelMapper.map(supplierRepository.save(supplierFromDB), SupplierDto.class);
     }
 
     @Override
     public void deleteSupplier(Integer id) {
-
+        jdbcTemplate.execute("delete from warehouse where supplier_id = " + id);
+        supplierRepository.deleteById(id);
     }
 }
